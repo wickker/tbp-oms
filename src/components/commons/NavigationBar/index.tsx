f93c190bb13f5ce3d95d92020ba7209d7c0d5b55
Dian Chun Choy <choydianchun@gmail.com>
2025-06-13 16:59:16 +0800

    [personal/dian]: wrap nav with SignedIn component

--- src/components/commons/NavigationBar/index.tsx
+++ src/components/commons/NavigationBar/index.tsx
@@ -1,5 +1,5 @@
 import { useLocation, useNavigate } from 'react-router-dom'
-import { SignOutButton } from '@clerk/clerk-react'
+import { SignedIn, SignOutButton } from '@clerk/clerk-react'
 import { Button } from '@/components/commons'
 import { cn } from '@/utils/functions'
 
@@ -19,37 +19,39 @@ const NavigationBar = () => {
   const navigate = useNavigate()
 
   return (
-    <div className='mb-2 h-[60px] w-screen min-w-[1450px] bg-black'>
-      <div className='mx-auto flex h-full w-full max-w-[1450px] items-center justify-between p-3 text-white'>
-        <div className='flex items-center gap-10'>
-          {navItems.map((item) => {
-            const isSelected = location.pathname === item.path
+    <SignedIn>
+      <div className='mb-2 h-[60px] w-screen min-w-[1450px] bg-black'>
+        <div className='mx-auto flex h-full w-full max-w-[1450px] items-center justify-between p-3 text-white'>
+          <div className='flex items-center gap-10'>
+            {navItems.map((item) => {
+              const isSelected = location.pathname === item.path
 
-            return (
-              <button
-                className='flex cursor-pointer flex-col items-center'
-                key={item.label}
-                onClick={() => navigate(item.path)}
-              >
-                <div className='flex items-center gap-x-1'>{item.label}</div>
-                <div
-                  className={cn(
-                    'h-[2px] w-full max-w-0 rounded-full transition-all',
-                    isSelected && 'max-w-full bg-white'
-                  )}
-                />
-              </button>
-            )
-          })}
+              return (
+                <button
+                  className='flex cursor-pointer flex-col items-center'
+                  key={item.label}
+                  onClick={() => navigate(item.path)}
+                >
+                  <div className='flex items-center gap-x-1'>{item.label}</div>
+                  <div
+                    className={cn(
+                      'h-[2px] w-full max-w-0 rounded-full transition-all',
+                      isSelected && 'max-w-full bg-white'
+                    )}
+                  />
+                </button>
+              )
+            })}
+          </div>
+
+          <SignOutButton>
+            <Button size='sm' variant='secondary'>
+              Sign Out
+            </Button>
+          </SignOutButton>
         </div>
-
-        <SignOutButton>
-          <Button size='sm' variant='secondary'>
-            Sign Out
-          </Button>
-        </SignOutButton>
       </div>
-    </div>
+    </SignedIn>
   )
 }
 
