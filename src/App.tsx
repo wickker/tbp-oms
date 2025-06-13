efc6acdfb2be5ec798f29b30ad207e6729d405c2
Dian Chun Choy <choydianchun@gmail.com>
2025-06-13 16:38:34 +0800

    [personal/dian]: add check auth component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Route, Routes } from 'react-router'
-import { ClerkProvider } from '@clerk/clerk-react'
+import { Navigate, Route, Routes } from 'react-router'
+import { ClerkProvider, useUser } from '@clerk/clerk-react'
+import { RiLoader4Fill } from 'react-icons/ri'
 import { NavigationBar } from '@/components/commons'
 import Config from '@/configs'
 import Customers from '@/views/Customers'
 import Main from '@/views/Main'
+
+const CheckAuth = () => {
+  const { isSignedIn, isLoaded } = useUser()
+
+  if (!isLoaded)
+    return (
+      <div className='flex min-h-[100dvh] min-w-max items-center justify-center overflow-y-hidden p-3'>
+        <RiLoader4Fill className='h-10 w-10 animate-spin text-neutral-300' />
+      </div>
+    )
+
+  if (!isSignedIn) return <Navigate to='/' />
+
+  return null
+}
 
 const App = () => {
   return (
@@ -11,6 +27,7 @@ const App = () => {
       publishableKey={Config.VITE_CLERK_PUBLISHABLE_KEY}
       afterSignOutUrl='/'
     >
+      <CheckAuth />
       <NavigationBar />
       <Routes>
         <Route path='/' element={<Main />} />
